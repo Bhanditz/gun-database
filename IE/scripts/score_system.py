a0d3699c33a15f11a291161e7e31fe52c780040f
epavlick <elliepavlick@gmail.com>
2016-05-19 07:53:11 -0400

    before completely refactoring scoring code

--- IE/scripts/score_system.py
+++ IE/scripts/score_system.py
@@ -10,12 +10,7 @@ if len(sys.argv) > 1:
   fuzzy = (sys.argv[1] == 'fuzzy')
 
 
-
-#9	Bridgeport man murdered in 9th homocide this year	{"circumstances":{"number-of-shots-fired":{"endIndex":1073,"startIndex":1071,"value":"3 "},"type-of-gun":{"endIndex":1115,"startIndex":1098,"value":"22 caliber pistol"}},"date-and-time":{"city":{"endIndex":59,"startIndex":49,"value":"Bridgeport"},"clock-time":{"endIndex":489,"startIndex":480,"value":"2:30 p.m."},"date":"2014-07-29","details":{"endIndex":470,"startIndex":435,"value":"near Reservoir and Trumbull avenues"},"state":"CT - Connecticut","time-day":{"endIndex":489,"startIndex":485,"value":"p.m."}},"radio1":{"The firearm was used during another crime.":"Not mentioned","The firearm was used in self defense.":"Not mentioned","The incident was a case of domestic violence.":"Not mentioned","The shooter and the victim knew each other.":"Not mentioned"},"radio2":{"Alcohol was involved.":"Not mentioned","Drugs (other than alcohol) were involved.":"Not mentioned","The shooting was a suicide or suicide attempt.":"Not mentioned","The shooting was self-directed.":"Not mentioned"},"radio3":{"The firearm was owned by the victim/victims family.":"Not mentioned","The firearm was stolen.":"Not mentioned","The shooting was by a police officer.":"Not mentioned","The shooting was directed at a police officer.":"No","The shooting was unintentional.":"Not mentioned"},"shooter-section":[],"victim-section":[{"age":{"endIndex":542,"startIndex":540,"value":"26"},"gender":"Male","name":{"endIndex":95,"startIndex":76,"value":"Christopher Pettway"},"race":{"endIndex":-1,"startIndex":-1,"value":""},"victim-was":["killed"]}]}
-
-#checkfields = [("victim-section", "victim-was"), ("victim-section", "name"), ("date-and-time", "time-day"), ("date-and-time", "details"), ("shooter-section", "name"), ("circumstances", "type-of-gun")]
 checkfields = [("victim-section", "name"), ("date-and-time", "time-day"), ("date-and-time", "details"), ("shooter-section", "name"), ("circumstances", "type-of-gun"), ("date-and-time", "date")]
-
 
 system = {}
 for line in open(systemfile).readlines():
@@ -30,8 +25,6 @@ def find_field(g, eid, section, field, value):
   was_found = set()
   attempted = set()
   if eid not in system:
-    #print "%s NA"%eid
-    #return
     return found, 0, total, None
   for d in system[eid]:
     s = get_value(section, field, d)
@@ -41,26 +34,87 @@ def find_field(g, eid, section, field, value):
 	gg = _gg[0] #ignore the indicies for now
 	attempted.add(gg)
         if gg not in was_found:	
-	  #if gg == 'Anna Roseberry':
-	  #  pdb.set_trace()
           for _ss in s:
 	    ss = _ss[0]
-	    if not fuzzy: 
-	      if gg == ss:
-	        found += 1
-	        was_found.add(gg)
-	    else:
-  	      if (gg in ss) or (ss in gg): 
-	        found += 1
-	        was_found.add(gg)
-  	      #elif (ss in gg): 
-	        #found += 1./len(gg.split())
-	        #was_found.add(gg)
-    #else:
-      #print "S is None"
+	    if is_match(gg, ss, field, section, fuzzy):
+	      found += 1
+	      was_found.add(gg)
+	    #else:
+              #if field == 'time-day': 
+	        #sys.stderr.write("Not equal %s | %s\n"%(gg, ss))
   skipped = len(g) - len(attempted)
   return found, skipped, total, s
-    
+  
+def date_eq(g, s, fuzzy=False):
+  dmy_g = g.split('-')
+  if len(dmy_g) < 3:
+    sys.stderr.write("Skipping %s | %s\n"%(g, s))
+    return True
+  dmy_s = s.split('-')
+  if not fuzzy:
+    if len(dmy_g) == len(dmy_s):
+      for ge, se in zip(dmy_g, dmy_s):
+        if not(ge == se):
+          #sys.stderr.write("Not equal %s | %s\n"%(g, s))
+  	  return False
+      return True
+    #sys.stderr.write("Not equal %s | %s\n"%(g, s))
+    return False
+  else:
+    years = False
+    months = False
+    days = False
+    #sys.stderr.write("%s | %s\n"%(dmy_g, dmy_s))
+    if dmy_g[0] == dmy_s[0] or dmy_g[0] == 'XXXX' or dmy_s[0] == 'XXXX': #year matches
+      years = True
+    if len(dmy_s) > 1 and ((dmy_g[1] == dmy_s[1]) or (dmy_g[1] == 'XX') or (dmy_s[1] == 'XX')): #months matches
+      months = True
+    if len(dmy_s) > 2 and ((dmy_g[2] == dmy_s[2]) or (dmy_g[2] == 'XX') or (dmy_s[2] == 'XX')): #days matches
+      days = True
+    if (years and months) or (months and days):
+      return True
+    return False
+
+def time_eq(g, s, fuzzy=False):
+  morning = set(['morning'])# 'am', 'a.m.'])
+  afternoon = set(['afternoon']) #, 'pm', 'p.m.'])
+  evening = set(['evening']) #, 'pm', 'p.m.'])
+  night = set(['night']) #, 'pm', 'p.m.'])
+  #sys.stderr.write("%s | %s\n"%(g, s))
+  if not fuzzy:
+    #Try to match time-day
+    gtoks = set(g.lower().split())
+    if s == 'MO':
+      return (len(gtoks.intersection(morning)) > 0)
+    if s == 'AF':
+      return (len(gtoks.intersection(afternoon)) > 0)
+    if s == 'EV':
+      return (len(gtoks.intersection(evening)) > 0)
+    if s == 'NI':
+      return (len(gtoks.intersection(night)) > 0)
+    #Try to match a clock time
+    gt = g.split()
+    st = s.split()
+    if len(gt) == len(st):
+      for ge, se in zip(gt, st):
+        if not(ge == se):
+          #sys.stderr.write("Not equal %s | %s\n"%(g, s))
+          return False
+      return True
+    #sys.stderr.write("Not equal %s | %s\n"%(g, s))
+    return False
+  else:
+    return False
+
+def is_match(g, s, field, section, fuzzy):
+  if field == 'date': 
+    return date_eq(g,s,fuzzy)
+  elif field == 'time-day' or field == 'clock-time':
+    return time_eq(g,s)
+  if fuzzy:
+    return ((g in s) or (s in g))
+  return g == s
+
 def get_value(section, field, data):
   gold = []
   #print section, field, data
@@ -78,12 +132,20 @@ def get_value(section, field, data):
           if not(g.strip() == ''):
             gold.append((g,sidx,eidx))
     else:
-      if field in data[section]:
-        g = data[section][field]['value'].strip().encode('utf-8')
-        sidx = data[section][field]['startIndex']
-        eidx = data[section][field]['endIndex']
-        if not(g.strip() == ''):
-          gold.append((g,sidx,eidx))
+      if True: #field in data[section]:
+	if field == 'date' and (isinstance(data[section][field], str) or isinstance(data[section][field], unicode)):
+          g = data[section][field].strip().encode('utf-8')
+          sidx = 1
+          eidx = 1
+          if not(g.strip() == ''):
+            gold.append((g,sidx,eidx))
+        else:
+	  #print section, field, data[section][field], type(data[section][field])
+          g = data[section][field]['value'].strip().encode('utf-8')
+          sidx = data[section][field]['startIndex']
+          eidx = data[section][field]['endIndex']
+          if not(g.strip() == ''):
+            gold.append((g,sidx,eidx))
 
   if len(gold) == 0:
     return None
@@ -102,5 +164,9 @@ for row in csv.DictReader(open(goldfile), delimiter='\t'):
       found, skipped, total, s = find_field(gold, eid, section, field, data)
       if field == 'details' and found == 0: #for location, try to match city if details failed
         found, skipped, total, s = find_field(gold, eid, section, 'city', data)
+      if field == 'time-day' and found == 0: #for time of day, try to match clock time if time of day failed
+        found, skipped, total, s = find_field(gold, eid, section, 'clock-time', data)
+	sys.stderr.write("Switched to clock time\n")
+        sys.stderr.write('\t'.join([eid, section, field, str(found), str(skipped), str(total), str(gold), str(s)])+'\n')
       print '\t'.join([eid, section, field, str(found), str(skipped), str(total), str(gold), str(s)])
   
